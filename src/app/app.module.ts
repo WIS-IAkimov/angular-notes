bf5f9008146b1774760a9cf1bd8111cf9b37a0e4
Iliya Akimov <i.akimov@wis.eu.com>
2018-07-23 15:27:05 +0300

    feat(header.component): create header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {
+  MatButtonToggleModule,
+  MatCardModule,
+  MatIconModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
+
 import { AppRoutingModule, appComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { LayoutModule } from '@angular/cdk/layout';
 
 
@@ -11,13 +23,22 @@ import { LayoutModule } from '@angular/cdk/layout';
   declarations: [
     AppComponent,
     appComponents,
+    HeaderComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
 
+    MatButtonToggleModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
     LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
